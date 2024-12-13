acc3611b82bf5fec3b0a8fcfb7d3fcdb6cb6cc66
Sashazach <zbostock26@gcds.net>
2024-12-12 22:45:14 -0500

    rolled back

--- index.py
+++ index.py
@@ -1,71 +1,62 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-import secrets
 import os
+import secrets
 from interests import INTERESTS
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = secrets.token_urlsafe(32)
-
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
+app.config['SECRET_KEY'] = secrets.token_urlsafe(32) # Important for security
+socketio = SocketIO(app, cors_allowed_origins="https://site-75299716458.northamerica-northeast1.run.app")
 
 states = ['ny', 'ma', 'ga', 'sc', 'pa', 'va']
 
-data = [[0 for _ in states] for _ in range(4)]
-
-current_text = ""
+data = [
+    [],
+    [],
+    [],
+    [],
+]
 
 @socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
+    emit('response', 'Server received: ' + message)
 
-@socketio.on('connect')
-def handle_connect():
-    emit('text_update', {'text': current_text})
-    print("A new client has connected. Current text sent.")
-    
-@socketio.on('text_update')
-def handle_text_update(data):
-    global current_text
-    current_text = data.get('text', '')
-    print("Received update to box:", current_text)
-    emit('text_update', data, broadcast=True, include_self=False)
-
+for i in range(len(data)):
+    for j in range(len(states)):
+        data[i].append(0)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-def state_page(state):
+def state(state):
     return_data = [states] + data
-    blurb = INTERESTS[states.index(state)]
-    return render_template('state.html', state=state, data=return_data, blurb=blurb)
+    return render_template('state.html', state=state, data = return_data, blurb=INTERESTS[states.index(state)])
 
-# Define routes for each state
 @app.route('/ny')
-def page_ny():
-    return state_page('ny')
+def page1():
+    return state('ny')
 
 @app.route('/ma')
-def page_ma():
-    return state_page('ma')
+def page2():
+    return state('ma')
 
 @app.route('/ga')
-def page_ga():
-    return state_page('ga')
+def page3():
+    return state('ga')
 
 @app.route('/sc')
-def page_sc():
-    return state_page('sc')
+def page4():
+    return state('sc')
 
 @app.route('/pa')
-def page_pa():
-    return state_page('pa')
+def page5():
+    return state('pa')
 
 @app.route('/va')
-def page_va():
-    return state_page('va')
+def page6():
+    return state('va')
 
 if __name__ == '__main__':
-    print("test")
-    app.run(debug=True, port=8080)
+    app.run(debug=True)
